c410ad3489bb800bc74408e0bf9c142dca80d1cc
Vanessa Martinez <vmar76@gmail.com>
2020-02-24 15:25:45 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,3 @@
+likely_to_rain = false
+garment = likely_to_rain ? "galoshes" : "sun hat"
+
---
+++ methods_and_arguments.rb
@@ -0,0 +1,12 @@
+#def greeting_a_person(name)
+#  puts "Hello #{name}"
+#end
+
+#greeting_a_person("Maria")
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("Maria", "Ruby")
+greeting_programmer("Vanessa", "Ruby")
